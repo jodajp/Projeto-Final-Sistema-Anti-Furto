bea97d1927e3004594e24de18e6d55a613c6d411
Toroyoshi <121224137+Toroyoshi@users.noreply.github.com>
2026-05-13 13:54:02 +0100

    att
    Vi um bug antes da normalização

--- pipeline/video_source.py
+++ pipeline/video_source.py
@@ -9,8 +9,8 @@ class VideoSource:
         if isinstance(source, str) and source.isdigit():
             source = int(source)
 
-        backend_name = self.config.get("backend", "CAP_ANY")
-        backend = getattr(cv2, backend_name, cv2.CAP_ANY)
+        backend_name = self.config.get("backend", "CAP_DSHOW")
+        backend = getattr(cv2, backend_name, cv2.CAP_DSHOW)
 
         cap = cv2.VideoCapture(source, backend)
 
